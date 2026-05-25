641eca9adc385f61ab0f571b99fbdfeb6bc4aefe
gembalnina-ux <gembalnina@gmail.com>
2026-05-25 17:25:42 +0200

    funkcja

---
+++ hello.py
@@ -0,0 +1,10 @@
+def function(x):
+    if x % 5 == 0:
+        return "sadge"
+    elif x % 3 == 0:
+        return "hello world"
+    else:
+        return "ani 3 ani 5"
+
+x = 9
+print(function(x))
